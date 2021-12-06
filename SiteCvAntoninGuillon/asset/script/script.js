0fd9f51f96b82794b63f70d48fd4941547f066fc
AntoninGuillon <91317448+AntoninGuillon@users.noreply.github.com>
2021-12-06 23:31:01 +0100

    Add files via upload

---
+++ SiteCvAntoninGuillon/asset/script/script.js
@@ -0,0 +1,12 @@
+
+
+
+function showProject(project){ //affiche une div de projet
+  project.className = "show";
+  project.style.display = "inline";
+}
+
+function hideProject(project){  //cache une div de projet
+  project.className = "hide";
+  project.style.display = "none";
+}
